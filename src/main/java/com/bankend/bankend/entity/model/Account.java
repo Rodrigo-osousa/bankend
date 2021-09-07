9b0d1e209ae2d62d83014adf9ebbe27efc5abb13
Rodrigo-osousa <rodrigo_oliveira_sousa@hotmail.com>
2021-09-07 18:40:28 -0300

    New Project

--- src/main/java/com/bankend/bankend/entity/model/Account.java
+++ src/main/java/com/bankend/bankend/entity/model/Account.java
@@ -1,7 +1,65 @@
 package com.bankend.bankend.entity.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idAccount;
-    private
+
+    private int agency;
+
+    private int balance;
+
+    private int credit;
+
+    public Account(){
+
+    }
+
+    public Account(int idAccount, int agency, int balance, int credit) {
+        this.idAccount = idAccount;
+        this.agency = agency;
+        this.balance = balance;
+        this.credit = credit;
+    }
+
+    public int getIdAccount() {
+        return idAccount;
+    }
+
+    public void setIdAccount(int idAccount) {
+        this.idAccount = idAccount;
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public void setAgency(int agency) {
+        this.agency = agency;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
 }
